15788d7e7f352105144a5330df580cfd9cd159df
Jaime Wyant <programmer.py@gmail.com>
2018-07-19 23:04:55 -0500

    Added actual program, and updated README.

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using WampSharp.CodeGeneration;
+
+// MOD: Import appropriate namespaces
+// Import the namespace containing the interfaces to be used for implementation
+// generation.  
+// using Your.Namespace.With.Proxies;
+
+namespace MkCalleeProxyClasses
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {            
+            // MOD: args = new string[] { "output-directory" };
+            // Set this to the full path of your output directory, or pass 
+            // the argument in via the command line.  I find it easier to just
+            // hardcode it here, and run the program whenever the proxy 
+            // interfaces change.
+            if (args.Length != 1)
+            {
+                Console.Error.WriteLine("Pass output directory on comd line");
+                return 1;
+            }
+
+            if (!Directory.Exists(args[0]))
+            {
+                Console.Error.WriteLine($"Directory {args[0]} does not exist.");
+                return 1;
+            }
+
+            // MOD: Generate an implementation file for each interface.
+            // One call to GenerateImplementation per interface.  Below is an example, change
+            // it.
+            GenerateImplentation(args[0], "SeasourcesApp.Wamp.Services", typeof(IAuthenticator));
+            
+            // MOD: Optionally generate a default interceptor.  If you don't generate one, you will
+            // need to create one on your own.
+            // GenerateInterceptor(args[0], "SeasourcesApp.Wamp.Services");
+            return 0;
+        }
+
+        static void GenerateImplentation(string outdir, string namespace_, Type type)
+        {
+            var codeGen = new CalleeProxyCodeGenerator(namespace_);
+            var code = codeGen.GenerateCode(type);
+            var interfaceName = GetInterfaceName(type);
+            var fileName = interfaceName + "Proxy.cs";
+            File.WriteAllText(Path.Combine(outdir, fileName), code);
+        }
+
+        static void GenerateInterceptor(string outdir, string namespace_)
+        {
+            var code = @"
+using System;
+using System.Reflection;
+using WampSharp.V2;
+using WampSharp.V2.Core.Contracts;
+using WampSharp.V2.Rpc;
+
+namespace XXX {
+    public class MyInterceptor : ICalleeProxyInterceptor
+        {
+            public CallOptions GetCallOptions(MethodInfo method)
+            {
+                return new CallOptions();
+            }
+
+            public string GetProcedureUri(MethodInfo method)
+            {
+                foreach (var customAttribute in method.CustomAttributes)
+                {
+                    if (customAttribute.AttributeType == typeof(WampProcedureAttribute)) 
+                    {
+                        return (string)customAttribute.ConstructorArguments[0].Value;       
+                    }
+
+                }
+                throw new Exception(""Could not find WampProcedureAttribute"");
+            }
+        }
+}
+";
+            File.WriteAllText(Path.Combine(outdir, "MyInterceptor.cs"), code.Replace("XXX", namespace_));
+        }
+
+        static string GetInterfaceName(Type interfaceType)
+        {
+            string result = interfaceType.Name;
+
+            if (result.StartsWith("I"))
+            {
+                return result.Substring(1);
+            }
+            else
+            {
+                return result;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # WampSharpProxyMaker
 Code generator for WampSharp Callee Proxies
+
+If you plan on using RPC via WAMP on iOS, then you need to generate proxies.
+Normally, WampSharp can generate them for you by calling GetCalleeProxy:
+
+```c#
+
+IArgumentsService proxy =
+                channel.RealmProxy.Services.GetCalleeProxy<IArgumentsService>();
+
+```
+
+Currently on iOS, this call generates an exception.  Elad has a better 
+understanding of this issue which he explains [here](https://github.com/Code-Sharp/WampSharp/issues/257)
+
+To get around this issue, create a dotnet core 2.0 console application within your solution.
+Copy Program.cs from this repository, into your project and modify it appropriately.
+
+I have marked the parts of the program that need to be modified with comments beginning with
+the string MOD:.  Below is the first one in the source code.
+
+```c#
+// MOD: Import appropriate namespaces
+```
+
+Run the program each time the proxy interface files change to generate new files.
